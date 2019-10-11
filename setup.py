fc56bff0c009d478e23b30add4304df87d11a0dc
dexter2206 <dexter2206@gmail.com>
2019-10-11 15:43:27 +0200

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""Setup file for dimodmock package"""
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dimodmock",
+    use_scm_version=True,
+    description="Utilities for mocking dimod Samplers",
+    author="Konrad Jałowiecki",
+    author_email="dexter2206@gmail.com",
+    packages=find_packages(exclude="tests"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["dimod"],
+    tests_require=["pytest", "pytest-mock"],
+    setup_requires=["setuptools_scm"],
+    keywords="dimod mock testing"
+)

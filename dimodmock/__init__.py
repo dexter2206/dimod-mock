17736e70c7bf86a472a8e4fe0fc70ad0980df020
dexter2206 <dexter2206@gmail.com>
2019-10-11 03:41:57 +0200

    Make StructuredMock derive from SamplerMock

--- dimodmock/__init__.py
+++ dimodmock/__init__.py
@@ -49,14 +49,14 @@ class SamplerMock(dimod.Sampler):
         return self._properties
 
 
-class StructuredMock(dimod.Sampler, dimod.Structured):
+class StructuredMock(SamplerMock, dimod.Structured):
     """Class mocking arbitrary-structured sampler."""
 
-    def __init__(self, nodelist: List[T], edgelist: List[Tuple[T, T]], properties: Dict[Any, Any], parameters: Dict[Any, List[Any]]):
+    def __init__(self, nodelist: List[T], edgelist: List[Tuple[T, T]], properties: Dict[Any, Any],
+                 parameters: Dict[Any, List[Any]]):
+        super().__init__(properties, parameters)
         self._nodelist = nodelist
         self._edgelist = edgelist
-        self._properties = properties
-        self._parameters = parameters
 
     def sample(self, bqm, **parameters):
         pass
@@ -70,19 +70,3 @@ class StructuredMock(dimod.Sampler, dimod.Structured):
     def edgelist(self) -> List[Tuple[T, T]]:
         """Returns list of all edges of this mock sampler."""
         return self._edgelist
-
-    @property
-    def parameters(self) -> Dict[Any, List[Any]]:
-        """Returns dict of parameters of this mock-sampler.
-
-        See dimod's documentation for further reference on this property.
-        """
-        return self._parameters
-
-    @property
-    def properties(self) -> Dict[Any, Any]:
-        """Returns dict of properties of this sampler.
-
-        See dimod's documentation for further reference on this parameter.
-        """
-        return self._properties

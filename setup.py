b302624124231309c3cb5528d8f31ae5a3f2ab7f
dexter2206 <dexter2206@gmail.com>
2019-10-11 17:19:15 +0200

    Add long description

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 """Setup file for dimodmock package"""
 from setuptools import setup, find_packages
 
+with open("README.md") as description_file:
+    long_description = description_file.read()
+
 
 setup(
     name="dimodmock",
     use_scm_version=True,
     description="Utilities for mocking dimod Samplers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Konrad Jałowiecki",
     author_email="dexter2206@gmail.com",
     packages=find_packages(exclude="tests"),
